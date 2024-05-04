21bfd0464d52f0179ca5dc963b222c6f2a453a80
nicolas <nicolas.programador@gmail.com>
2024-05-03 20:53:48 -0400

    formulario creado

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,10 +1,26 @@
 
 $(document).ready(function(){
 
-    console.log("Ok");
+    
+    let numero1 = "";
 
+
+    $("#btnSumar").click(function(){
+        
+        //numero1 = document.getElementById("numero1").value;
+        numero1 = $("#numero1").val();
+        if(numero1 === ""){
+            $("#error1").text("Error1");
+            $("#error1").css("color","red");
+            return;
+        }
+
+
+    })
 
 
 
 
 });
+
+//numero1 = document.getElementById("numero1").value;
